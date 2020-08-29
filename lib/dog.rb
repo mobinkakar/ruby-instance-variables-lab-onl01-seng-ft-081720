148d9de97ef5c8ce1ea72681d9c8bd36f44e7a85
Mobin Kakar <mobin.kakar@wsabronx.org>
2020-08-29 14:25:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,9 @@
 class Dog
-def bark
-  puts "Woof!"
-end
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+  end
 
-def sit
-  puts "The Dog is sitting"
-end
+  def name
+    @this_dogs_name
+  end
 end
